86c6164da11c111ac2c7ecd14de4141071ca89db
Jaime González Fábregas <jaimegonzalezfabregas@gmail.com>
2022-02-17 18:50:37 +0100

    muxa cosa

--- static/main.js
+++ static/main.js
@@ -1,148 +1,124 @@
-// import * as THREE from './../node_modules/three/build/three.module.js';
 import * as THREE from '/three';
-// import * as THREE from '/three';
-
-
+import { StereoEffect } from './../node_modules/three/examples/jsm/effects/StereoEffect.js';
 import { GLTFLoader } from './../node_modules/three/examples/jsm/loaders/GLTFLoader.js';
-//import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import DeviceOrientationControls from './gyro_cam.js';
+
+const width_factor = 0.9;
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color('black');
+scene.background = new THREE.Color('grey');
 
-const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.01, 1000);
-
-console.log(camera);
+const camera = new THREE.PerspectiveCamera(75, innerWidth * width_factor/ innerHeight, 0.01, 1000);
+let controls = new DeviceOrientationControls(camera);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setPixelRatio(devicePixelRatio)
-renderer.setSize(innerWidth, innerHeight);
+renderer.setSize(innerWidth*width_factor, innerHeight);
 renderer.outputEncoding = THREE.sRGBEncoding
+
+let effect = new StereoEffect(renderer);
+effect.setSize(window.innerWidth * width_factor, window.innerHeight);
+
 document.body.appendChild(renderer.domElement);
+
 
 const loader = new GLTFLoader();
 
-loader.load('untitled.gltf', function (gltf) {
+loader.load('untitled.gltf', (gltf) => {
+	const root = gltf.scene;
 
-  scene.add(gltf.scene);
+	// root.rotateY(-89.55);
 
+	// root.position.set(0, -0.7, 0);
+	root.castShadow = true;
+	gltf.scene.traverse(function (node) {
+		if (node instanceof THREE.Mesh) {
+			node.castShadow = true;
+		}
+	})
 
+	scene.add(gltf.scene);
 }, undefined, function (error) {
 
-  console.error(error);
+	console.error(error);
 
 });
+
+
 
 
 const light = new THREE.HemisphereLight('white', // bright sky color
-  'darkslategrey', // dim ground color
-  0.3, // intensity
+	'darkslategrey', // dim ground color
+	0.3, // intensity
 )
-console.log(light)
 scene.add(light)
-console.log(scene)
 
-// const light2 = new THREE.PointLight(0xffffff, 1);
-// light2.position.set(0, 10, 0)
-// scene.add(light2)
+let keys = [];
+{
+	let ws_url = "ws://" + location.host + "" + 1;
+	console.log(ws_url)
+	const ws = new WebSocket(ws_url)
 
-var keys = [];
-var ratony = 0;
-var ratonx = 0;
+	ws.onmessage = (msg) => {
+		keys = JSON.parse(msg.data)
+	};
+}
 
-let ws_url = "ws://" + location.host + "" + 1;
-console.log(ws_url)
-const ws = new WebSocket(ws_url)
-
-ws.onmessage = (msg) => {
-  keys = JSON.parse(msg.data)
-};
 
 
 let time = 0
-let speed = 0.1;
 function animate() {
-  time++;
+	time++;
+	controls.update();
 
-  var cameraFacing = new THREE.Vector3(0, 0, - 1);
-  var cameraUp = new THREE.Vector3(0, 1, 0);
-  var cameraRight = new THREE.Vector3(-1, 0, 0);
+	let cameraFacing = new THREE.Vector3(0, 0, - 1);
+	let cameraUp = new THREE.Vector3(0, 1, 0);
+	let cameraRight = new THREE.Vector3(-1, 0, 0);
 
-  cameraFacing.applyQuaternion(camera.quaternion);
-  cameraUp.applyQuaternion(camera.quaternion);
-  cameraRight.applyQuaternion(camera.quaternion);
+	cameraFacing.applyQuaternion(camera.quaternion);
+	cameraUp.applyQuaternion(camera.quaternion);
+	cameraRight.applyQuaternion(camera.quaternion);
 
-
-  if (keys[87]) { // w
-    camera.position.add(cameraFacing);
-  }
-  if (keys[83]) { // s
-    camera.position.sub(cameraFacing);
-  }
-  if (keys[65]) { // a
-    camera.position.add(cameraRight);
-  }
-  if (keys[68]) { // d
-    camera.position.sub(cameraRight);
-  }
-  if (keys[32]) { // space
-    camera.position.add(cameraUp);
-  }
-  if (keys[160]) { // shift
-    camera.position.sub(cameraUp);
-  }
-
-
-
-  renderer.render(scene, camera);
-  requestAnimationFrame(animate);
-
-
-
-
+	if (keys[87]) { // w
+		camera.position.add(cameraFacing);
+	}
+	if (keys[83]) { // s
+		camera.position.sub(cameraFacing);
+	}
+	if (keys[65]) { // a
+		camera.position.add(cameraRight);
+	}
+	if (keys[68]) { // d
+		camera.position.sub(cameraRight);
+	}
+	if (keys[32]) { // space
+		camera.position.add(cameraUp);
+	}
+	if (keys[160]) { // shift
+		camera.position.sub(cameraUp);
+	}
+	effect.render(scene, camera);
+	requestAnimationFrame(animate);
 }
 
+renderer.domElement.addEventListener("click", () => {
+	renderer.domElement.requestFullscreen();
+	renderer.setPixelRatio(devicePixelRatio)
+	renderer.setSize(innerWidth * width_factor, innerHeight);
+	effect.setSize(window.innerWidth * width_factor, window.innerHeight);
+	onWindowResize()
 
+})
 
+function onWindowResize() {
 
-
-window.addEventListener('mousemove', function onMouseover(e) {
-  ratonx = e.clientX;
-  ratony = e.clientY;
-});
-
-window.addEventListener('DOMMouseScroll', mouseWheelEvent);
-window.addEventListener("wheel", mouseWheelEvent);
-function mouseWheelEvent(e) {
-  // console.log(e.wheelDelta ? e.wheelDelta : -e.detail);
-  var movement = (e.wheelDelta ? e.wheelDelta : -e.detail);
-  if (movement < 0) {
-    ScrollUp();
-  } else {
-    ScrollDown();
-  }
-}
-
-function ScrollUp() {
+	camera.aspect = window.innerWidth * width_factor / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setPixelRatio(devicePixelRatio)
+	renderer.setSize(innerWidth * width_factor, innerHeight);
+	effect.setSize(window.innerWidth * width_factor, window.innerHeight);
 
 }
-function ScrollDown() {
-
-}
-
-
-function handleOrientation(event) {
-  var x = event.beta;  // In degree in the range [-180,180)
-  var y = event.gamma + 90; // In degree in the range [-90,90)
-  let z = event.alpha;
-
-  console.log(x, y);
-  camera.rotation._x = -y / 180 * Math.PI;
-  camera.rotation._y = x / 180 * Math.PI;
-  camera.rotation._z = z / 180 * Math.PI;
-
-  camera.rotation._onChangeCallback();
-}
-
-window.addEventListener('deviceorientation', handleOrientation);
+window.onresize = onWindowResize;
 
 animate()
